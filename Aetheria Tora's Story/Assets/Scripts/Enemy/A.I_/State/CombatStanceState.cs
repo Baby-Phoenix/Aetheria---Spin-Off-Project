0536e2764dbf10368dc8c311356f626b4f710b31
dabbydoo <fakhri.pranayanda@ontariotechu.net>
2023-04-06 06:56:44 -0400

    Ai vfx and vfx pack

--- Aetheria Tora's Story/Assets/Scripts/Enemy/A.I_/State/CombatStanceState.cs
+++ Aetheria Tora's Story/Assets/Scripts/Enemy/A.I_/State/CombatStanceState.cs
@@ -8,13 +8,20 @@ public class CombatStanceState : State
     public AttackState attackState;
     public PursueTargetState pursueTargetState;
     public LayerMask detectionLayer;
+    public ParticleFade particleFade;
+    public GameObject enemyModel;
+    public Collider enemyCollider;
+    public Canvas enemyCanvas;
 
     public float teleportRadius = 5.0f;
     public float teleportChance = 0.1f;
     public float maxHeightDifference = 2.0f;
     public float teleportCooldown = 10.0f;
-    public bool canTeleport = false;
     private float lastTeleportTime;
+    public float teleportDelay = 2f;
+    private bool isTeleporting = false;
+    public bool canTeleport;
+
 
     private void Start()
     {
@@ -64,7 +71,7 @@ public class CombatStanceState : State
 
     public void HandleTeleportation(EnemyManager enemyManager)
     {
-        if (canTeleport && Time.time - lastTeleportTime >= teleportCooldown)
+        if (canTeleport && Time.time - lastTeleportTime >= teleportCooldown && !isTeleporting)
         {
             if (Random.value < teleportChance)
             {
@@ -88,21 +95,77 @@ public class CombatStanceState : State
                         return;
                     }
 
-                    // Check if the destination has line of sight to the target
-                    //if (Physics.Linecast(navHit.position, enemyManager.currentTarget.transform.position, out hit) && hit.collider.tag != "Player")
-                    //{
-                    //    print("!Player");
-                    //    // Destination does not have line of sight to the target, skip teleport
-                    //    return this;
-                    //}
+                    //particleFade.FadeInAndOut();
+                    //enemyManager.navmeshAgent.Warp(navHit.position);
+                    //enemyManager.transform.position = navHit.position;
+                    //lastTeleportTime = Time.time;
+                    StartCoroutine(FadeInAndOutCoroutine(enemyManager, navHit.position));
 
-                    enemyManager.navmeshAgent.Warp(navHit.position);
-                    enemyManager.transform.position = navHit.position;
-                    lastTeleportTime = Time.time;
+
                 }
             }
         }
     }
+
+
+
+    private IEnumerator FadeInAndOutCoroutine(EnemyManager enemyManager, Vector3 teleportPosition)
+    {
+        lastTeleportTime = Time.time;
+        particleFade.FadeInAndOut();
+
+        isTeleporting = true;
+        yield return new WaitForSeconds(teleportDelay);
+        enemyModel.SetActive(false);
+        enemyCollider.enabled = false;
+        enemyCanvas.gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(3);
+
+        enemyManager.navmeshAgent.Warp(teleportPosition);
+        enemyManager.transform.position = teleportPosition;
+
+        
+
+        particleFade.FadeInAndOut();
+
+        yield return new WaitForSeconds(3);
+
+        enemyModel.SetActive(true);
+        enemyCollider.enabled = true;
+        enemyCanvas.gameObject.SetActive(true);
+
+        isTeleporting = false;
+    }
+
+    //private IEnumerator FadeOutCoroutine(EnemyManager enemyManager, Vector3 teleportPosition)
+    //{
+    //    yield return new WaitForSeconds(teleportDelay);
+    //    enemyModel.SetActive(false);
+    //    enemyCollider.enabled = false;
+    //    enemyCanvas.gameObject.SetActive(false);
+    //    enemyManager.navmeshAgent.Warp(teleportPosition);
+    //    enemyManager.transform.position = teleportPosition; 
+
+    //    while (particleFade.getIsFadeOut())
+    //    {
+    //        yield return null;
+    //    }
+    //    isFadingOut = false;
+    //    FadeInCoroutine(enemyManager, teleportPosition);
+    //}
+
+    //private IEnumerator FadeInCoroutine(EnemyManager enemyManager, Vector3 teleportPosition)
+    //{
+    //    while (particleFade.getIsFadeIn())
+    //    {
+    //        yield return null;
+    //    }
+    //    enemyModel.SetActive(true);
+    //    enemyCollider.enabled = true;
+    //    enemyCanvas.gameObject.SetActive(true);
+    //    isFadingOut = true;
+    //}
 
 
     public void HandleRotateTowardsTarget(EnemyManager enemyManager)
